e43652fba48dd47f3c66c9bbcfea5bb3fe9b7e19
Babylatta <111289746+Babylatta@users.noreply.github.com>
2023-02-26 00:35:08 +0900

    #4.7 Super Recap

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function onLoginSubmit(event) {
    loginForm.classList.add(HIDDEN_CLASSNAME);
    const username = loginInput.value;
    localStorage.setItem(USERNAME_KEY,username);
-   paintGreetings(username);
+   paintGreetings();
 }
 
 function paintGreetings(username) {
@@ -26,6 +26,6 @@ if (savedUsername === null) {
    loginForm.classList.remove(HIDDEN_CLASSNAME);
    loginForm.addEventListener("submit",onLoginSubmit);
 } else {
-   paintGreetings(savedUsername);
+   paintGreetings();
 
 }
